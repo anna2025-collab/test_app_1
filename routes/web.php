e58b6f249f4e48275a4258316bb6a9fb3e7c1dc5
anna2025-collab <korolevaanna2025@gmail.com>
2026-07-16 22:18:16 +0400

    Serve landing pages without Blade compilation

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('landing');
+    return response(File::get(resource_path('views/landing.blade.php')))
+        ->header('Content-Type', 'text/html; charset=UTF-8');
 });
 
 Route::get('/docs', function () {
-    return view('docs');
+    return response(File::get(resource_path('views/docs.blade.php')))
+        ->header('Content-Type', 'text/html; charset=UTF-8');
 });
